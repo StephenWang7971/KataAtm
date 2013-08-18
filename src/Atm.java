4c7881741c8bf280af6c1a1c58c215baa1a35786
StephenWang <stephen.wang.7971@gmail.com>
2013-08-18 14:33:58 +0800

    apply with masks.

--- src/Atm.java
+++ src/Atm.java
@@ -20,24 +20,38 @@ public class Atm {
     }
 
     public void onCommand(Command command) {
-       switch(command.code) {
-           case Command.CHECK_BALANCE:
-               System.out.println("remain balance is " + account.getAmount());
-               break;
-           case Command.DEPOSIT:
-               //TODO assert param is a plus.
-               account.addAmount(command.param);
-               System.out.println( "deposit " + command.param + ", remain balance is " + account.getAmount());
-               break;
-           case Command.WITH_DRAW:
-               //TODO assert param is a plus.
-               account.addAmount(-command.param);
-               System.out.println( "withdraw " + command.param + ", remain balance is " + account.getAmount());
-               break;
-           case Command.MASK_MUNICIPAL:
-               account.addAmount(-command.param * 2);
-               System.out.println( "buy " + command.param + " unit of electricity, remain balance is " + account.getAmount());
-               break;
+
+        if ((command.code & Command.MASK_BANKING) == Command.MASK_BANKING) {
+            processBanking(command);
+        } else if ((command.code & Command.MASK_MUNICIPAL) == Command.MASK_MUNICIPAL) {
+            processMunicipal(command);
+        }
+
+    }
+
+    public void processBanking(Command command) {
+        switch(command.code) {
+        case Command.CHECK_BALANCE:
+            System.out.println("remain balance is " + account.getAmount());
+            break;
+        case Command.DEPOSIT:
+            //TODO assert param is a plus.
+            account.addAmount(command.param);
+            System.out.println( "deposit " + command.param + ", remain balance is " + account.getAmount());
+            break;
+        case Command.WITH_DRAW:
+            //TODO assert param is a plus.
+            account.addAmount(-command.param);
+            System.out.println( "withdraw " + command.param + ", remain balance is " + account.getAmount());
+            break;
+        }
+    }
+    public void processMunicipal(Command command) {
+        switch (command.code) {
+            case Command.MUNICIPAL_ELECTRICITY:
+                account.addAmount(-command.param * 2);
+                System.out.println( "buy " + command.param + " unit of electricity, remain balance is " + account.getAmount());
+                break;
        }
     }
 }
